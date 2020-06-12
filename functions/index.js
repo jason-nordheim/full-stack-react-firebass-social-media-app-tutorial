d3e099db3841eb0305516fba209c6571c6db4c93
jason-nordheim <jason.nordheim@gmail.com>
2020-06-12 14:58:33 -0600

    added post request for creating a new scream

--- functions/index.js
+++ functions/index.js
@@ -21,3 +21,22 @@ exports.getScreams = functions.https.onRequest((request, response) => {
         })
         .catch(error => console.error(error.message))
 })
+
+
+exports.createScream = functions.https.onRequest((request, response) => {
+   const newScream = {
+       body: request.body.body, 
+       userHandle: request.body.userHandle, 
+       createdAt: admin.firestore.Timestamp.fromDate(new Date())
+   }
+
+   admin.firestore().collection("screams").add(newScream)
+    .then(doc => {
+        response.json({message: `document ${doc.id} created successfully`})
+    })
+    .catch(err => {
+        // server error 
+        response.status(500).json({error: 'something went wrong'})
+        console.error(err)
+    })
+})
